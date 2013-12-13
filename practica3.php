d3a28fb114107b3236d7f7f00cba27cbc2d80459
miguelluna93 <miguelitoo13@hotmail.com>
2013-12-12 19:37:00 -0600

    practica3

---
+++ practica3.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <link rel="stylesheet" href="PRACTICA3.css">
+ <?php
+ 
+  $filas = 10;
+  $columnas = 10;
+  $numero = 0;
+  ?>                                                   
+ </head>                                                                 
+ <body>      
+ 
+<table border="1">
+ <?php
+
+ $clase='';
+ for($f=0;$f<$filas;$f++){
+  echo "<tr>";
+ 
+  for($y=0;$y<$columnas;$y++){
+ 
+   if($f%2==0){
+    $clase='class="par"';
+    }else{
+$clase='class="inpar"';
+   }
+  echo "<td ".$clase.">".$numero."</td>";
+  $numero++;
+  }
+   echo "</tr>";
+  }
+ ?>
+ 
+ </table>
+ </body>                                                                 
+ </html>
